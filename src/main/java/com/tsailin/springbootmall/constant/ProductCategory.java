354a55e3e645558803af900128288508a8bf065f
Tsailin_Yuan <swchloetl@gmail.com>
2023-11-05 20:40:11 +0800

    Product function : Rafactor category into Enum - ProductCategory

---
+++ src/main/java/com/tsailin/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,15 @@
+package com.tsailin.springbootmall.constant;
+
+public enum ProductCategory {
+	FRESH_PRODUCE,
+	MEAT_AND_SEAFOOD,
+	DELI,
+	SNACKS,
+	PANTRY,
+	BEVERAGES,
+	BAKERY,
+	DAIRY_AND_EGGS,
+	CANDY,
+	COFFEE,
+	TEA
+}
