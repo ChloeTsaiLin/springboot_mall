ec78cd48765420fbb2199414ba4f5eaecdf25fbf
Tsailin_Yuan <swchloetl@gmail.com>
2023-11-08 16:37:05 +0800

    Product function : implement query product-list (getProducts) (sort)

--- src/main/java/com/tsailin/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/tsailin/springbootmall/dao/impl/ProductDaoImpl.java
@@ -33,13 +33,16 @@ public class ProductDaoImpl implements ProductDao{
 		Map<String, Object> map = new HashMap<>();
 		
 		if(productQueryParams.getCategory() != null) {
-			sql = sql + " AND category = :category";
+			sql += " AND category = :category";
 			map.put("category", productQueryParams.getCategory().name());
 		}
 		if(productQueryParams.getSearch() != null) {
 			sql += " AND product_name LIKE :search";
 			map.put("search", "%" + productQueryParams.getSearch() + "%");
 		}
+		
+			sql += " ORDER BY " + productQueryParams.getOrderBy()
+				+ " " +productQueryParams.getSort(); 
 		
 		List<Product> productList = nameParameterJdbcTemplate
 									.query(sql, map, new ProductRowMapper());
