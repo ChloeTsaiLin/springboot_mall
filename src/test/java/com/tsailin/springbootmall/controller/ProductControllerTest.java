da04d00c4d2d62ded8fea0953ffbe6fb577a26df
Tsailin_Yuan <swchloetl@gmail.com>
2024-01-04 23:17:36 +0800

    Product function : unit test (getProduct, createProduct)

---
+++ src/test/java/com/tsailin/springbootmall/controller/ProductControllerTest.java
@@ -0,0 +1,144 @@
+package com.tsailin.springbootmall.controller;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tsailin.springbootmall.constant.ProductCategory;
+import com.tsailin.springbootmall.dto.ProductRequest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    
+	
+    //Get Product
+	@Test
+	void testGetProduct_success() throws Exception{
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/products/{productId}", 1);
+		
+		mockMvc.perform(requestBuilder)
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.productName").value("Apple"))
+			.andExpect(jsonPath("$.category").value("FRESH_PRODUCE"))
+			.andExpect((ResultMatcher) jsonPath("$.imageUrl", notNullValue()))
+			.andExpect((ResultMatcher) jsonPath("$.price", notNullValue()))
+			.andExpect((ResultMatcher) jsonPath("$.stock", notNullValue()))
+			.andExpect((ResultMatcher) jsonPath("$.productDesc", notNullValue()))
+			.andExpect((ResultMatcher) jsonPath("$.createdDate", notNullValue()))
+			.andExpect((ResultMatcher) jsonPath("$.lastModifiedDate", notNullValue()));
+				
+	}
+	
+	@Test
+	void testGetProduct_notFound() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products/{productId}", 2000);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(404));
+	}
+	
+
+	// Create Product
+	@Test
+	void testCreateProduct_success() throws Exception {
+		ProductRequest productRequest = new ProductRequest();
+		productRequest.setProductName("test create product");
+		productRequest.setCategory(ProductCategory.BEVERAGES);
+		productRequest.setImageUrl("http://testCreateProduct.jpg");
+		productRequest.setPrice(222);
+		productRequest.setStock(20);
+		productRequest.setProductDesc("test create product: description.");
+		
+		String json = objectMapper.writeValueAsString(productRequest);
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.post("/products")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json);
+		
+		mockMvc.perform(requestBuilder)
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.productName").value("test create product"))
+			.andExpect(jsonPath("$.category").value("BEVERAGES"))
+			.andExpect((ResultMatcher) jsonPath("$.imageUrl", notNullValue()))
+			.andExpect((ResultMatcher) jsonPath("$.price").value(222))
+			.andExpect((ResultMatcher) jsonPath("$.stock").value(20))
+			.andExpect((ResultMatcher) jsonPath("$.productDesc").value("test create product: description."))
+			.andExpect((ResultMatcher) jsonPath("$.createdDate", notNullValue()))
+			.andExpect((ResultMatcher) jsonPath("$.lastModifiedDate", notNullValue()));
+		
+	}
+	
+	@Test
+	void testCreateProduct_illegalArgument() throws Exception {
+		ProductRequest productRequest = new ProductRequest();
+		productRequest.setProductName("test create product");
+		productRequest.setPrice(222);
+		productRequest.setStock(20);
+		
+		String json = objectMapper.writeValueAsString(productRequest);
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.post("/products")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json);
+		
+		mockMvc.perform(requestBuilder)
+			.andExpect(status().isBadRequest());
+	}
+
+	
+//	//Update Product
+//	@Test
+//	void testUpdateProduct_success() throws Exception{
+//		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/products/{productId}", 2)
+//	}
+//
+//	@Test
+//	void testUpdateProduct_illegalArgument() throws Exception{
+//		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/products/{productId}", 3)
+//	}
+//
+//	@Test
+//	void testUpdateProduct_productNotFound() throws Exception{
+//		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/products/{productId}", 20000)
+//	}
+//	
+//	//Delete Product
+//	@Test
+//	void testDeleteProduct_success() throws Exception{
+//		RequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/products/{productId}", 1)
+//	}
+//	
+//	@Test
+//	void testDeleteProduct_NonExistingProduct() throws Exception{
+//		RequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/products/{productId}", 1)
+//	}
+//	
+//    //Get Product-list
+//  @Test
+//  void testGetProducts_success() throws Exception{
+//		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/products");
+//  }
+
+}
---
+++ src/test/resources/data.sql
@@ -0,0 +1,26 @@
+-- product
+INSERT INTO product (product_name, category, image_url, price, stock, product_desc, created_date, last_modified_date) 
+	VALUES ('Apple', 'FRESH_PRODUCE', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/992px-Red_Apple.jpg', 
+	20, 30, 'Treat your family to the wildly juicy, sweet and crisp taste of Envy Apples. With a classic apple cider flavor, these apples have a firm texture with flesh that is slow to brown. This variety was developed in New Zealand by master farmers and now grown in Zillah, Washington.', 
+	'2023-11-02 02:41:28', '2023-11-02 22:41:28');
+INSERT INTO product (product_name, category, image_url, price, stock, product_desc, created_date, last_modified_date) 
+	VALUES 	('Avocados', 'FRESH_PRODUCE', 'https://m.media-amazon.com/images/I/51FXx8p9rSL._AC_SX522_.jpg', 
+	25, 50, 'Avocados can be used in Mexican food items like tacos or burritos, as appetizers like avocado crostini, or in a fresh guacamole or avocado dip. They provide unsaturated "good" fats and almost 20 vitamins, minerals, and phytonutrients.', 
+	'2023-11-07 12:41:28', '2023-11-07 13:35:28'); 
+INSERT INTO product (product_name, category, image_url, price, stock, product_desc, created_date, last_modified_date) 
+	VALUES 	('Snickers Iced Coffee Latte', 'COFFEE', 'https://m.media-amazon.com/images/I/81JDSDZm-vL._SX679_PIbundle-12,TopRight,0,0_SX679SY433SH20_.jpg',
+	65, 40, ' Reach for this rich and delicious blend of Victor Allen’s Snickers Iced Coffee made with real milk and cream for the perfect blend of coffee and flavor!', 
+	'2023-11-07 12:41:28', '2023-11-07 13:35:28'); 
+INSERT INTO product (product_name, category, image_url, price, stock, product_desc, created_date, last_modified_date) 
+	VALUES ('Fresh Broccoli', 'FRESH_PRODUCE', 'https://static.hkej.net/lj/images/2016/12/14/1449083_9e7c846103113dc81a083495775dfb53_940.jpg',
+	65, 40, 'Broccoli crowns are the premium cut of broccoli, trimmed just under the broccoli head. Healthy & Delicious!', 
+	'2023-11-07 12:41:28', '2023-11-07 13:35:28');
+INSERT INTO product (product_name, category, image_url, price, stock, created_date, last_modified_date)
+	VALUES ('Avocados(Mexico)', 'FRESH_PRODUCE', 'https://m.media-amazon.com/images/I/51FXx8p9rSL._AC_SX522_.jpg', 
+	29, 100, '2023-12-07 12:41:28', '2024-01-03 11:35:28'); 
+INSERT INTO product (product_name, category, image_url, price, stock, created_date, last_modified_date)
+	VALUES ('Instant Coffee Crystals', 'COFFEE', 'https://a.rimg.com.tw/s3/gmarket/bf9/306/agent%40gmarket/2/52/2b/808325a32c04be638ad5f29ae2773772_32021000493611.jpg', 
+	300, 80, '2023-11-07 14:07:08', '2023-11-07 14:15:08');
+INSERT INTO product (product_name, category, image_url, price, stock, created_date, last_modified_date)
+	VALUES ('Premium Instant Coffee: Medium Roast', 'COFFEE', 'https://www.ringtons.co.uk/images/ringtons-premium-gold-instant-coffee-100g-p27-5309_medium.jpg', 
+	488, 180, '2023-11-07 14:07:08', '2023-11-07 14:15:08');
