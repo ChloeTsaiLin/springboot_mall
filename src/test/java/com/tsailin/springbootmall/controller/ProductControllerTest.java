43b9d65377504ffaec3782a814b4341b19f05e61
Tsailin_Yuan <swchloetl@gmail.com>
2024-01-23 17:47:42 +0800

    Product function : unit test (updateProduct, deleteProduct)

--- src/test/java/com/tsailin/springbootmall/controller/ProductControllerTest.java
+++ src/test/java/com/tsailin/springbootmall/controller/ProductControllerTest.java
@@ -108,34 +108,89 @@ public class ProductControllerTest {
 	}
 
 	
-//	//Update Product
-//	@Test
-//	void testUpdateProduct_success() throws Exception{
-//		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/products/{productId}", 2)
-//	}
-//
-//	@Test
-//	void testUpdateProduct_illegalArgument() throws Exception{
-//		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/products/{productId}", 3)
-//	}
-//
-//	@Test
-//	void testUpdateProduct_productNotFound() throws Exception{
-//		RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/products/{productId}", 20000)
-//	}
-//	
-//	//Delete Product
-//	@Test
-//	void testDeleteProduct_success() throws Exception{
-//		RequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/products/{productId}", 1)
-//	}
-//	
-//	@Test
-//	void testDeleteProduct_NonExistingProduct() throws Exception{
-//		RequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/products/{productId}", 1)
-//	}
-//	
-//    //Get Product-list
+	//Update Product
+	@Test
+	void testUpdateProduct_success() throws Exception{
+		ProductRequest productRequest = new ProductRequest();
+		productRequest.setProductName("test update product");
+		productRequest.setCategory(ProductCategory.BEVERAGES);
+		productRequest.setImageUrl("http://testCreateProduct.jpg");
+		productRequest.setPrice(222);
+		productRequest.setStock(20);
+		productRequest.setProductDesc("test update product: description.");
+		
+		String json = objectMapper.writeValueAsString(productRequest);
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.put("/products/{productId}", 2)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json);
+		
+		mockMvc.perform(requestBuilder)
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.productName").value("test update product"))
+			.andExpect(jsonPath("$.category").value("BEVERAGES"))
+			.andExpect(jsonPath("$.imageUrl").value("http://testCreateProduct.jpg"))
+			.andExpect(jsonPath("$.price").value(222))
+			.andExpect(jsonPath("$.stock").value(20))
+			.andExpect(jsonPath("$.productDesc").value("test update product: description."))
+			.andExpect((ResultMatcher) jsonPath("$.createdDate", notNullValue()))
+			.andExpect((ResultMatcher) jsonPath("$.lastModifiedDate", notNullValue()));
+	}
+
+	@Test
+	void testUpdateProduct_illegalArgument() throws Exception{
+		ProductRequest productRequest = new ProductRequest();
+		productRequest.setProductName("test update product");
+		
+		String json = objectMapper.writeValueAsString(productRequest);
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.put("/products/{productId}", 3)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json);
+		
+		mockMvc.perform(requestBuilder)
+			.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	void testUpdateProduct_productNotFound() throws Exception{
+		ProductRequest productRequest = new ProductRequest();
+		productRequest.setProductName("test update product");
+		
+		String json = objectMapper.writeValueAsString(productRequest);
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.put("/products/{productId}", 20000)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json);
+		
+		mockMvc.perform(requestBuilder)
+			.andExpect(status().isBadRequest());
+	}
+	
+	//Delete Product
+	@Test
+	void testDeleteProduct_success() throws Exception{
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.delete("/products/{productId}", 4);
+		
+		mockMvc.perform(requestBuilder)
+			.andExpect(status().isNoContent());
+	}
+	
+	@Test
+	void testDeleteProduct_NonExistingProduct() throws Exception{
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.delete("/products/{productId}", 2000);
+		
+		mockMvc.perform(requestBuilder)
+			.andExpect(status().isNoContent());
+	}
+	
+    //Get Product-list
 //  @Test
 //  void testGetProducts_success() throws Exception{
 //		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/products");
